1394f4e990a4a4e1e5565ec1e6096531269b0f9e
juanda <juandavidgo1997@gmail.com>
2026-01-15 13:09:39 -0500

    Implement ESLint and Peettier

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-    });
+  console.log(`Listening on port ${PORT}...`);
+});
--- src/app.js
+++ src/app.js
@@ -2,14 +2,8 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//     console.log(`Listening on port ${PORT}...`);
-//     });
-
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from Acquisitions!');
+  res.status(200).send('Hello from Acquisitions!');
 });
 
 export default app;
