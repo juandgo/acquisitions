a76f3a0594a0c3c4e356536ccd96cfcd9a9ea0ce
juanda <juandavidgo1997@gmail.com>
2026-01-17 13:36:48 -0500

    Everything working

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,12 @@
 import logger from '#config/logger.js';
 import { signupSchema } from '#validations/auth.validation.js';
 import { formatValidationErrors } from '#utils/format.js';
+import { createUser } from '#services/auth.service.js';
+import { cookies } from '#utils/cookies.js';
+import { jwttoken } from '#utils/jwt.js';
 
-export const signup = (req, res, next) => {
+export const signup = async (req, res, next) => {
+  console.log('BODY:', req.body);
   try{
     const validationResult = signupSchema.safeParse(req.body);
 
@@ -13,22 +17,28 @@ export const signup = (req, res, next) => {
       });
     }
 
-    const { name, email, role } = validationResult.data;
+    const { name, email, password, role } = validationResult.data;
 
-    // AUTH SERVICE
+    const user = await createUser({name, email, password, role });
+    
+    const token = jwttoken.sign({ id: user.id, email: user.email, role: user.role });
+
+    cookies.set(res, 'token', token);
 
     logger.info(`User registered succesfully: ${email}`);
     res.status(201).json({
       message: 'User registered successfully',
-      user: { id: 1, name, email, role }
+      user: { id: user.id, name: user.name, email: user.email, role: user.role}
     });
 
   }catch(e){
     logger.error('Signup error', e);
-    if(e.message === 'User with this email already exists'){
-      return res.status(409).json({error: 'Email already in use'});
-    }
-    next(e);
+    // if(e.message === 'User with this email already exists'){
+    //   return res.status(409).json({error: 'Email already in use'});
+    // }
+    return res.status(e.message === 'User with this email already exists' ? 409 : 500)
+      .json({ error: e.message });
+    // next(e);
   }
 
 };
